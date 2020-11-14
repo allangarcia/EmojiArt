99f4f5cb5441464e0d44f634450b80cdf44e0e25
Allan Garcia <allan.garcia@gmail.com>
2020-11-14 18:55:34 -0300

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Allan Garcia on 14/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
