bff1cb7f17d54eec1dee82c8e27a0945078c61e1
Allan Garcia <allan.garcia@gmail.com>
2020-11-16 12:04:39 -0300

    Jsonification and storing in UserDefaults

--- EmojiArt/EmojiArtDocument.swift
+++ EmojiArt/EmojiArtDocument.swift
@@ -11,7 +11,22 @@ class EmojiArtDocument: ObservableObject {
     
     static let palette: String = "🦁🐸🐤🍅🍎🎱🥎🏀💣🧨"
     
-    @Published private var emojiArt: EmojiArt = EmojiArt()
+    // @Published [WORKAROUND]
+    private var emojiArt: EmojiArt = EmojiArt() {
+        willSet {
+            objectWillChange.send()
+        }
+        didSet {
+            UserDefaults.standard.set(emojiArt.json, forKey: EmojiArtDocument.untitled)
+        }
+    }
+    
+    private static let untitled = "EmojiArtDocument.Untitled"
+    
+    init() {
+        emojiArt = EmojiArt(json: UserDefaults.standard.data(forKey: EmojiArtDocument.untitled)) ?? EmojiArt()
+        fetchBackgroundImageData()
+    }
     
     @Published private(set) var backgroundImage: UIImage?
     
